bb16e39c42975e72e6f63734fa78aa5e2480c220
Pavel Lobashov <shockwavenn@gmail.com>
2019-07-12 13:20:16 +0300

    Call configure on docker-compose

--- configure.sh
+++ configure.sh
@@ -2,9 +2,11 @@
 
 set -x
 
+/usr/bin/owncloud server &> /tmp/server.log &
 cp -r /tmp/onlyoffice /var/www/owncloud/custom/
 chown -R www-data:www-data /var/www/owncloud/custom/onlyoffice
 occ --no-warnings app:enable onlyoffice
 occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
 occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-document-server/"
 occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nginx-server/"
+tail -f /tmp/server.log
