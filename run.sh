864e88d194804d963a2bd3f68236a3b030acffb8
Pavel Lobashov <shockwavenn@gmail.com>
2019-07-12 13:33:29 +0300

    Add timeout for owncloud initialization

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 set -x
 
 /usr/bin/owncloud server &> /tmp/server.log &
+sleep 10 # owncloud should init itself before ability to enable app
 cp -r /tmp/onlyoffice /var/www/owncloud/custom/
 chown -R www-data:www-data /var/www/owncloud/custom/onlyoffice
 occ --no-warnings app:enable onlyoffice
