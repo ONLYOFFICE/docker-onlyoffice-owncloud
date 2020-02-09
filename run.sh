9293ddb39f2d40066af2d6cdf5828118c34298c0
K Kaushal <you@example.com>
2020-02-09 17:53:47 +0000

    docker shared directory fix

--- run.sh
+++ run.sh
@@ -4,8 +4,11 @@ set -x
 
 /usr/bin/owncloud server &> /tmp/server.log &
 sleep 10 # owncloud should init itself before ability to enable app
-cp -r /tmp/onlyoffice /var/www/owncloud/custom/
-chown -R www-data:www-data /var/www/owncloud/custom/onlyoffice
+ln -s /mnt/data/files /var/www/owncloud/files
+#cp -r /tmp/onlyoffice /var/www/owncloud/files/
+#cp -r /tmp/onlyoffice /var/www/owncloud/custom/
+#chown -R www-data:www-data /var/www/owncloud/custom/onlyoffice
+chown -R www-data:www-data /var/www/owncloud/files/onlyoffice
 occ --no-warnings app:enable onlyoffice
 occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
 occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-document-server/"
